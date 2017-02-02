7e7c334897114776093f178e180c096179db5f9f
Vasily Nabatchikov <Vasily.Nabatchikov@emc.com>
2017-02-02 13:20:11 +0300

    channel all 'new' requests within memory.hpp via one function [IRES-403]

--- core/utils/memory.hpp
+++ core/utils/memory.hpp
@@ -20,64 +20,6 @@
 
 #include "shared.hpp"
 
-#define DECLARE_SPTR( class_name ) typedef std::shared_ptr< class_name > ptr
-#define DECLARE_PTR(class_name) typedef std::unique_ptr< class_name > ptr
-#define DECLARE_REF(class_name) typedef std::reference_wrapper<class_name> ref
-#define DECLARE_CREF(class_name) typedef std::reference_wrapper<const class_name> cref
-
-#define DECLARE_FACTORY(class_name) \
-template< typename _Ty, typename... _Args > \
-static ptr make(_Args&&... args) { \
-  typedef typename std::enable_if< \
-    std::is_base_of< class_name, _Ty >::value, \
-    _Ty>::type type; \
-  return ptr(new type( \
-    std::forward< _Args >(args)... \
-  )); \
-}
-
-//////////////////////////////////////////////////////////////////////////////
-/// @brief default implementation of a factory method, instantiation on heap
-///        NOTE: make(...) MUST be defined in CPP to ensire proper code scope
-//////////////////////////////////////////////////////////////////////////////
-#define DECLARE_FACTORY_DEFAULT(...) static ptr make(__VA_ARGS__);
-#define DEFINE_FACTORY_DEFAULT(class_type) \
-/*static*/ class_type::ptr class_type::make() { \
-  return class_type::ptr(new class_type()); \
-}
-#define DEFINE_FACTORY_POOLED(class_type) \
-/*static*/ class_type::ptr class_type::make() { \
-  static const size_t free_list_empty = std::numeric_limits<size_t>::max(); \
-  static size_t free_list_head = free_list_empty; \
-  static std::mutex mutex; \
-  static std::deque<std::pair<class_type, size_t>> pool; \
-  class_type::ptr::element_type* entry; \
-  size_t entry_pos; \
-  std::lock_guard<std::mutex> lock(mutex); \
-  if (free_list_empty == free_list_head) { \
-    entry_pos = pool.size(); \
-    entry = &(pool.emplace(pool.end(), class_type(), free_list_empty)->first); \
-  } else { \
-    auto& entry_pair = pool[free_list_head]; \
-    entry = &(entry_pair.first); \
-    entry_pos = free_list_head; \
-    free_list_head = entry_pair.second; \
-  } \
-  return class_type::ptr( \
-    entry, \
-    [entry_pos](class_type::ptr::element_type*)->void { \
-      std::lock_guard<std::mutex> lock(mutex); \
-      pool[entry_pos].second = free_list_head; \
-      free_list_head = entry_pos; \
-    } \
-  ); \
-} // use std::deque as a non-reordering block-reserving container (user should #include all required dependencies)
-#define DEFINE_FACTORY_SINGLETON(class_type) \
-/*static*/ class_type::ptr class_type::make() { \
-  static class_type::ptr instance(new class_type()); \
-  return instance; \
-}
-
 NS_ROOT
 NS_BEGIN( memory )
 
@@ -154,9 +96,81 @@ NS_END
 
 #define PTR_NAMED__(line, class_type, name, ...) \
   auto ptr ## line = iresearch::memory::make_unique<char[]>(sizeof(class_type)); \
-  class_type::ptr name(::new(ptr ## line.get()) class_type(__VA_ARGS__)); \
-  reinterpret_cast<class_type*>(ptr ## line.release()); \
-  name
+  ::new(ptr ## line.get()) class_type(__VA_ARGS__); \
+  class_type::ptr name(reinterpret_cast<class_type*>(ptr ## line.release()))
 #define PTR_NAMED(class_type, name, ...) PTR_NAMED__(__LINE__, class_type, name, __VA_ARGS__)
+
+#define DECLARE_SPTR(class_name) typedef std::shared_ptr<class_name> ptr
+#define DECLARE_PTR(class_name) typedef std::unique_ptr<class_name> ptr
+#define DECLARE_REF(class_name) typedef std::reference_wrapper<class_name> ref
+#define DECLARE_CREF(class_name) typedef std::reference_wrapper<const class_name> cref
+
+#define DECLARE_FACTORY(class_name) \
+template<typename _T, typename... _Args> \
+static ptr make(_Args&&... args) { \
+  typedef typename std::enable_if<std::is_base_of<class_name, _T>::value, _T>::type type; \
+  auto instance = iresearch::memory::make_unique<char[]>(sizeof(type)); \
+  ::new(instance.get()) type(std::forward<_Args>(args)...); \
+  return ptr(reinterpret_cast<type*>(instance.release())); \
+}
+
+//////////////////////////////////////////////////////////////////////////////
+/// @brief default implementation of a factory method, instantiation on heap
+///        NOTE: make(...) MUST be defined in CPP to ensire proper code scope
+//////////////////////////////////////////////////////////////////////////////
+#define DECLARE_FACTORY_DEFAULT(...) static ptr make(__VA_ARGS__);
+#define DEFINE_FACTORY_DEFAULT(class_type) \
+/*static*/ class_type::ptr class_type::make() { \
+  PTR_NAMED(class_type, ptr); \
+  return ptr; \
+}
+
+//////////////////////////////////////////////////////////////////////////////
+/// @brief implementation of a factory method, using a deque to store and
+///        reuse instances with the help of a skip-list style offset free_list
+///        use std::deque as a non-reordering block-reserving container
+///        user should #include all required dependencies e.g. <deque>,<mutex>
+///        NOTE: make(...) MUST be defined in CPP to ensire proper code scope
+//////////////////////////////////////////////////////////////////////////////
+#define DEFINE_FACTORY_POOLED(class_type) \
+/*static*/ class_type::ptr class_type::make() { \
+  static const size_t free_list_empty = std::numeric_limits<size_t>::max(); \
+  static size_t free_list_head = free_list_empty; \
+  static std::mutex mutex; \
+  static std::deque<std::pair<class_type, size_t>> pool; \
+  class_type::ptr::element_type* entry; \
+  size_t entry_pos; \
+  std::lock_guard<std::mutex> lock(mutex); \
+  if (free_list_empty == free_list_head) { \
+    entry_pos = pool.size(); \
+    entry = &(pool.emplace(pool.end(), class_type(), free_list_empty)->first); \
+  } else { \
+    auto& entry_pair = pool[free_list_head]; \
+    entry = &(entry_pair.first); \
+    entry_pos = free_list_head; \
+    free_list_head = entry_pair.second; \
+  } \
+  return class_type::ptr( \
+    entry, \
+    [entry_pos](class_type::ptr::element_type*)->void { \
+      std::lock_guard<std::mutex> lock(mutex); \
+      pool[entry_pos].second = free_list_head; \
+      free_list_head = entry_pos; \
+    } \
+  ); \
+}
+
+//////////////////////////////////////////////////////////////////////////////
+/// @brief implementation of a factory method, returning a singleton instance
+///        NOTE: make(...) MUST be defined in CPP to ensire proper code scope
+//////////////////////////////////////////////////////////////////////////////
+#define DEFINE_FACTORY_SINGLETON(class_type) \
+/*static*/ class_type::ptr class_type::make() { \
+  struct make_impl_t { \
+    static class_type::ptr make() { PTR_NAMED(class_type, ptr); return ptr; } \
+  }; \
+  static auto instance = make_impl_t::make(); \
+  return instance; \
+}
 
 #endif
