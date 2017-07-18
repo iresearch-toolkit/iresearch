cda957d7e600203df70a31284d1413153ec1fc5c
Andrey Abramov <andrey@arangodb.com>
2017-07-18 18:58:42 +0300

    do not clear 'score' attribute in basic_doc_iterator::score

--- core/search/conjunction.hpp
+++ core/search/conjunction.hpp
@@ -43,6 +43,7 @@ struct score_wrapper : BaseWrapper {
     : BaseWrapper(std::move(base)) {
     auto& scr = (*this)->attributes().template get<iresearch::score>();
     if (scr) {
+      scr->clear();
       score = scr->c_str();
     }
   }
