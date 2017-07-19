a7895cb2a061ac7f8bede2aae4872660a1db7b4e
Andrey Abramov <andrey@arangodb.com>
2017-07-19 20:05:52 +0300

    extend all/empty filter tests with cost checking

--- tests/search/all_filter_tests.cpp
+++ tests/search/all_filter_tests.cpp
@@ -22,7 +22,7 @@ namespace tests {
 class all_filter_test_case : public filter_test_case_base {
 protected:
   void all_sequential() {
-    /* add segment */
+    // add segment
     {
       tests::json_doc_generator gen(
          resource("simple_sequential.json"),
@@ -32,11 +32,10 @@ protected:
 
     auto rdr = open_reader();
 
-    check_query(
-      ir::all( ),
-      docs_t{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 },
-      rdr
-    );
+    docs_t docs{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 };
+    std::vector<irs::cost::cost_t> cost{ docs.size() };
+
+    check_query(ir::all(), docs, cost, rdr);
   }
 }; // all_filter_test_case
 
