866887942b3b0c67bcb9d220a668c1ca70ee7049
Vasiliy <vasiliy@arangodb.com>
2017-09-28 20:01:03 +0300

    add support for building lz4 directly from source

--- cmake/FindLz4.cmake
+++ cmake/FindLz4.cmake
@@ -13,38 +13,90 @@ if ("${LZ4_ROOT}" STREQUAL "")
   endif()
 endif()
 
-set(LZ4_SEARCH_HEADER_PATHS
-  ${LZ4_ROOT}/include
-)
+if (NOT "${LZ4_ROOT}" STREQUAL "")
+  set(LZ4_SEARCH_HEADER_PATHS
+    ${LZ4_ROOT}
+    ${LZ4_ROOT}/include
+    ${LZ4_ROOT}/include/lz4
+    ${LZ4_ROOT}/include
+  )
 
-set(LZ4_SEARCH_LIB_PATH
-  ${LZ4_ROOT}/lib
-)
+  set(LZ4_SEARCH_LIB_PATHS
+    ${LZ4_ROOT}
+    ${LZ4_ROOT}/lib
+  )
 
-if(NOT MSVC AND "${LZ4_ROOT}" STREQUAL "")
+  set(LZ4_SEARCH_SRC_PATHS
+    ${LZ4_ROOT}
+    ${LZ4_ROOT}/lib
+    ${LZ4_ROOT}/src
+    ${LZ4_ROOT}/src/lz4
+    ${LZ4_ROOT}/src/lz4/lib
+  )
+elseif (NOT MSVC)
   set(UNIX_DEFAULT_INCLUDE
       "/usr/include"
+      "/usr/include/lz4"
       "/usr/include/x86_64-linux-gnu"
+      "/usr/include/x86_64-linux-gnu/lz4"
   )
-endif()
 
-find_path(Lz4_INCLUDE_DIR 
-  lz4.h 
-  PATHS ${LZ4_SEARCH_HEADER_PATHS} ${UNIX_DEFAULT_INCLUDE}
-  NO_DEFAULT_PATH # make sure we don't accidentally pick up a different version
-)
-
-include(Utils)
-
-if(NOT MSVC AND "${LZ4_ROOT}" STREQUAL "")
   set(UNIX_DEFAULT_LIB
       "/lib"
       "/lib/x86_64-linux-gnu"
       "/usr/lib"
       "/usr/lib/x86_64-linux-gnu"
   )
+
+  set(UNIX_DEFAULT_SRC
+      "/usr/src"
+      "/usr/src/lz4"
+      "/usr/src/lz4/lib"
+  )
 endif()
 
+find_path(Lz4_INCLUDE_DIR
+  lz4.h
+  PATHS ${LZ4_SEARCH_HEADER_PATHS} ${UNIX_DEFAULT_INCLUDE}
+  NO_DEFAULT_PATH # make sure we don't accidentally pick up a different version
+)
+
+find_path(Lz4_SRC_DIR_LZ4
+  lz4.c
+  PATHS ${LZ4_SEARCH_SRC_PATHS} ${UNIX_DEFAULT_SRC}
+  NO_DEFAULT_PATH # make sure we don't accidentally pick up a different version
+)
+
+if (Lz4_SRC_DIR_LZ4)
+  get_filename_component(Lz4_SRC_DIR_PARENT ${Lz4_SRC_DIR_LZ4} DIRECTORY)
+
+  find_path(Lz4_SRC_DIR_CMAKE
+    CMakeLists.txt
+    PATHS ${Lz4_SRC_DIR_LZ4} ${Lz4_SRC_DIR_PARENT}/contrib/cmake_unofficial
+    NO_DEFAULT_PATH # make sure we don't accidentally pick up a different version
+  )
+endif()
+
+# found the cmake enabled source directory
+if (Lz4_INCLUDE_DIR AND Lz4_SRC_DIR_LZ4 AND Lz4_SRC_DIR_CMAKE)
+  set(Lz4_FOUND TRUE)
+
+  # use of BUILD_SHARED_LIBS BUILD_STATIC_LIBS has no effect
+  set(LZ4_BUNDLED_MODE OFF) # enable lz4_shared
+  set(BUILD_SHARED_LIBS ON) # enable lz4_static
+  add_subdirectory(
+    ${Lz4_SRC_DIR_CMAKE}
+    ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/iresearch-lz4.dir
+  )
+
+  set(Lz4_LIBRARY_DIR ${LZ4_SEARCH_LIB_PATHS})
+  set(Lz4_SHARED_LIB lz4_shared)
+  set(Lz4_STATIC_LIB lz4_static)
+
+  return()
+endif()
+
+include(Utils)
 
 # set options for: shared
 if (MSVC)
@@ -62,7 +114,7 @@ set_find_library_options("${LZ4_LIBRARY_PREFIX}" "${LZ4_LIBRARY_SUFFIX}")
 # find library
 find_library(Lz4_SHARED_LIB
   NAMES lz4
-  PATHS ${LZ4_SEARCH_LIB_PATH} ${UNIX_DEFAULT_LIB}
+  PATHS ${LZ4_SEARCH_LIB_PATHS} ${UNIX_DEFAULT_LIB}
   NO_DEFAULT_PATH
 )
 
@@ -83,7 +135,7 @@ set_find_library_options("${LZ4_LIBRARY_PREFIX}" "${LZ4_LIBRARY_SUFFIX}")
 # find library
 find_library(Lz4_STATIC_LIB
   NAMES lz4
-  PATHS ${LZ4_SEARCH_LIB_PATH} ${UNIX_DEFAULT_LIB}
+  PATHS ${LZ4_SEARCH_LIB_PATHS} ${UNIX_DEFAULT_LIB}
   NO_DEFAULT_PATH
 )
 
@@ -94,20 +146,20 @@ restore_find_library_options()
 if (Lz4_INCLUDE_DIR AND Lz4_SHARED_LIB AND Lz4_STATIC_LIB)
   set(Lz4_FOUND TRUE)
   set(Lz4_LIBRARY_DIR
-    "${LZ4_SEARCH_LIB_PATH}"
+    "${LZ4_SEARCH_LIB_PATHS}"
     CACHE PATH
     "Directory containing lz4 libraries"
     FORCE
   )
 
-  add_library(lz4-shared IMPORTED SHARED)
-  set_target_properties(lz4-shared PROPERTIES
+  add_library(lz4_shared IMPORTED SHARED)
+  set_target_properties(lz4_shared PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${Lz4_INCLUDE_DIR}"
     IMPORTED_LOCATION "${Lz4_SHARED_LIB}"
   )
 
-  add_library(lz4-static IMPORTED STATIC)
-  set_target_properties(lz4-static PROPERTIES
+  add_library(lz4_static IMPORTED STATIC)
+  set_target_properties(lz4_static PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${Lz4_INCLUDE_DIR}"
     IMPORTED_LOCATION "${Lz4_STATIC_LIB}"
   )
