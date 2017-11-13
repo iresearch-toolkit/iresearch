5c823b33d5b4f02d5885f0ccd22def27f18886cb
Vasiliy <vasiliy@arangodb.com>
2017-11-13 17:54:44 +0300

    increase test timeouts to address intermitent runtime failures on Jenkins

--- tests/utils/object_pool_tests.cpp
+++ tests/utils/object_pool_tests.cpp
@@ -127,7 +127,7 @@ TEST_F(object_pool_tests, bounded_sobject_pool) {
     {
       SCOPED_LOCK_NAMED(mutex, lock);
       std::thread thread([&cond, &mutex, &pool]()->void{ auto obj = pool.emplace(2); SCOPED_LOCK(mutex); cond.notify_all(); });
-      ASSERT_NE(std::cv_status::no_timeout, cond.wait_for(lock, std::chrono::milliseconds(7000))); // assume thread blocks in 7000ms (3000ms is not enough for MSVC2017@appveyor, 6000ms is not enough for MSVC2017@jenkins)
+      ASSERT_NE(std::cv_status::no_timeout, cond.wait_for(lock, std::chrono::milliseconds(8000))); // assume thread blocks in 8000ms (3000ms is not enough for MSVC2017@appveyor, 7000ms is not enough for MSVC2017@jenkins)
       obj.reset();
       lock.unlock();
       thread.join();
@@ -268,7 +268,7 @@ TEST_F(object_pool_tests, bounded_uobject_pool) {
       SCOPED_LOCK(mutex);
       cond.notify_all();
     });
-    auto result = cond.wait_for(lock, std::chrono::milliseconds(1000)); // assume thread finishes in 1000ms
+    auto result = cond.wait_for(lock, std::chrono::milliseconds(2000)); // assume thread finishes in 1000ms  // assume thread blocks in 2000ms (1000ms is not enough for MSVC2017@appveyor)
     obj.reset();
 
     if (lock) {
