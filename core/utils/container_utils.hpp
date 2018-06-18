3b6916e7e4fde2df5968639d8ca0be5bbe257fb4
Vasiliy <vasiliy@arangodb.com>
2018-06-18 12:24:00 +0300

    try to address MSVC2017 build failure on appveyor

--- core/utils/container_utils.hpp
+++ core/utils/container_utils.hpp
@@ -236,7 +236,7 @@ MSVC_ONLY(__pragma(warning(pop)))
 NS_BEGIN(memory)
 
 template<typename BucketFactory, size_t Size>
-class IRESEARCH_API_TEMPLATE bucket_allocator {
+class IRESEARCH_API_TEMPLATE bucket_allocator: private util::noncopyable { // noncopyable because of 'pools_'
  public:
   // number of pools
   static const size_t SIZE = Size;
