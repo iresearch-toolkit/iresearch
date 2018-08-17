81b1376bb5fc37a5f33450b59ebef95dbe7d7e03
Vasiliy <vasiliy@arangodb.com>
2018-08-17 15:54:54 +0300

    remove redundant functions

--- core/index/index_writer.hpp
+++ core/index/index_writer.hpp
@@ -177,48 +177,12 @@ class IRESEARCH_API index_writer : util::noncopyable {
   /// @param func the insertion logic
   /// @return all fields/attributes successfully insterted
   ////////////////////////////////////////////////////////////////////////////
-  template<typename Func>
-  bool update(const irs::filter& filter, Func func) {
+  template<typename Filter, typename Func>
+  bool update(Filter&& filter, Func func) {
     auto ctx = get_flush_context(); // retain lock until end of update(...)
     auto writer = get_segment_context(*ctx);
 
-    writer->begin(make_update_context(*ctx, filter));
-
-    return update(*ctx, *writer, func);
-  }
-
-  ////////////////////////////////////////////////////////////////////////////
-  /// @brief replaces documents matching filter with the document
-  ///        to be filled by the specified functor
-  /// @note that changes are not visible until commit()
-  /// @param filter the document filter
-  /// @param func the insertion logic
-  /// @return all fields/attributes successfully insterted
-  ////////////////////////////////////////////////////////////////////////////
-  template<typename Func>
-  bool update(irs::filter::ptr&& filter, Func func) {
-    auto ctx = get_flush_context(); // retain lock until end of update(...)
-    auto writer = get_segment_context(*ctx);
-
-    writer->begin(make_update_context(*ctx, std::move(filter)));
-
-    return update(*ctx, *writer, func);
-  }
-
-  ////////////////////////////////////////////////////////////////////////////
-  /// @brief replaces documents matching filter with the document
-  ///        to be filled by the specified functor
-  /// @note that changes are not visible until commit()
-  /// @param filter the document filter
-  /// @param func the insertion logic
-  /// @return all fields/attributes successfully insterted
-  ////////////////////////////////////////////////////////////////////////////
-  template<typename Func>
-  bool update(const std::shared_ptr<irs::filter>& filter, Func func) {
-    auto ctx = get_flush_context(); // retain lock until end of update(...)
-    auto writer = get_segment_context(*ctx);
-
-    writer->begin(make_update_context(*ctx, filter));
+    writer->begin(make_update_context(*ctx, std::forward<Filter>(filter)));
 
     return update(*ctx, *writer, func);
   }
