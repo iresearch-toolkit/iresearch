7f6ea03eeb2028affe5a559eddc9d71038681f40
Andrey Abramov <andrey@arangodb.com>
2019-07-19 18:16:28 +0300

    reset transaction tick

--- core/index/index_writer.cpp
+++ core/index/index_writer.cpp
@@ -722,6 +722,8 @@ index_writer::documents_context::~documents_context() NOEXCEPT {
 }
 
 void index_writer::documents_context::reset() NOEXCEPT {
+  tick_ = 0; // reset tick
+
   auto& ctx = segment_.ctx();
 
   if (!ctx) {
