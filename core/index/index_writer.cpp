093beb063bc245616a6698e608762a4e7ff266cb
Andrei Lobov <andrei.lobov@arangodb.com>
2019-12-23 21:01:15 +0300

    fix typo

--- core/index/index_writer.cpp
+++ core/index/index_writer.cpp
@@ -2086,7 +2086,7 @@ index_writer::pending_context_t index_writer::flush_all(const before_commit_f& b
     // write non-empty document mask
     if (!docs_mask.empty()) {
       if (!pending_consolidation) { // if this is pending consolidation this segment is already in the mask (see assert below)
-        //new version will be created created. Remove old version from cache!
+        //new version will be created. Remove old version from cache!
         ctx->segment_mask_.emplace(pending_segment.segment.meta);
       }
       assert(ctx->segment_mask_.find(pending_segment.segment.meta) != ctx->segment_mask_.end()); // this segment should be deleted from cache!
