bfaa25f9cc69b7b319e0f3e159f4f00087267619
Andrey Abramov <andrey@arangodb.com>
2019-12-25 17:08:12 +0300

    add tests for utf8_utils

--- core/utils/utf8_utils.hpp
+++ core/utils/utf8_utils.hpp
@@ -54,39 +54,20 @@ FORCE_INLINE const byte_type* next(const byte_type* it, const byte_type* end) no
   return it > end ? end : it;
 }
 
-FORCE_INLINE uint32_t length(uint32_t cp) noexcept {
-  if (cp < 0x80) {
-    return 1;
-  } else if ((cp >> 5) == 0x60){
-    return 2;
-  } else if ((cp >> 4) == 0xE0) {
-    return 3;
-  } else if ((cp >> 3) == 0x1E) {
-    return 4;
-  }
-
-  return 0;
-}
-
 FORCE_INLINE uint32_t next(const byte_type*& it) noexcept {
   IRS_ASSERT(it);
 
   uint32_t cp = *it;
 
-  switch (length(cp)) {
-    case 1: break;
-    case 2:
-      cp = ((cp << 6) & 0x7FF) + (uint32_t(*++it) & 0x3F);
-      break;
-    case 3:
-      cp = ((cp << 12) & 0xFFFF) + ((uint32_t(*++it) << 6) & 0xFFF);
-      cp += (*++it) & 0x3F;
-      break;
-    case 4:
-      cp = ((cp << 18) & 0x1FFFFF) + ((uint32_t(*++it) << 12) & 0x3FFFF);
-      cp += (uint32_t(*++it) << 6) & 0xFFF;
-      cp += uint32_t(*++it) & 0x3F;
-      break;
+  if ((cp >> 5) == 0x06) {
+    cp = ((cp << 6) & 0x7FF) + (uint32_t(*++it) & 0x3F);
+  } else if ((cp >> 4) == 0x0E) {
+    cp = ((cp << 12) & 0xFFFF) + ((uint32_t(*++it) << 6) & 0xFFF);
+    cp += (*++it) & 0x3F;
+  } else if ((cp >> 3) == 0x1E) {
+    cp = ((cp << 18) & 0x1FFFFF) + ((uint32_t(*++it) << 12) & 0x3FFFF);
+    cp += (uint32_t(*++it) << 6) & 0xFFF;
+    cp += uint32_t(*++it) & 0x3F;
   }
 
   ++it;
@@ -96,7 +77,7 @@ FORCE_INLINE uint32_t next(const byte_type*& it) noexcept {
 
 template<typename OutputIterator>
 inline void to_utf8(const bytes_ref& in, OutputIterator out) {
-  for (auto begin = in.begin(), end = in.end(); begin != end; ) {
+  for (auto begin = in.begin(), end = in.end(); begin != end; ++out) {
     *out = next(begin);
   }
 }
