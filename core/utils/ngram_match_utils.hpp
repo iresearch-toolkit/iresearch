19d7828b292d230d2431437dcea99e0bd486d7a9
Andrei Lobov <andrei.lobov@arangodb.com>
2020-03-04 18:34:54 +0300

    added ngram postion similarity measure

--- core/utils/ngram_match_utils.hpp
+++ core/utils/ngram_match_utils.hpp
@@ -29,7 +29,8 @@
 
 NS_ROOT
 
-template<typename T>
+
+template<typename T, bool use_ngram_position_match>
 float_t ngram_similarity(const T* lhs, size_t lhs_size,
                          const T* rhs, size_t rhs_size,
                          size_t ngram_size) {
@@ -38,15 +39,15 @@ float_t ngram_similarity(const T* lhs, size_t lhs_size,
     return 0.f;
   }
 
-  const size_t lhs_ngram_length = lhs_size - ngram_size + 1;
-  const size_t rhs_ngram_length = rhs_size - ngram_size + 1;
+  const size_t lhs_ngram_count = lhs_size - ngram_size + 1;
+  const size_t rhs_ngram_count = rhs_size - ngram_size + 1;
 
   const T* lhs_ngram_start = lhs;
   
   const T* lhs_ngram_start_end  = lhs + lhs_size - ngram_size + 1; // <end> for ngram start
 
-  float_t prev_best = 0;
-  std::vector<float_t> S(std::max(lhs_ngram_length, rhs_ngram_length) + 1, 0);
+  float_t d = 0;
+  std::vector<float_t> cache(std::max(lhs_ngram_count, rhs_ngram_count) + 1, 0);
 
   size_t lhs_ngram_idx = 1;
   for (; lhs_ngram_start != lhs_ngram_start_end; ++lhs_ngram_start, ++lhs_ngram_idx) {
@@ -58,24 +59,34 @@ float_t ngram_similarity(const T* lhs, size_t lhs_size,
     for (; rhs_ngram_start != rhs_ngram_start_end; ++rhs_ngram_start, ++rhs_ngram_idx) {
       const T* rhs_ngram_end = rhs_ngram_start + ngram_size;
       // boolean similarity by now. Just match or not!
-      float_t similarity = 1;
+      float_t similarity = use_ngram_position_match ? 0 : 1;
       for (const T* l = lhs_ngram_start, *r = rhs_ngram_start; l != lhs_ngram_end && r != rhs_ngram_end; ++l, ++r) {
-        if (*l != *r) {
-          similarity = 0;
-          break;
+        if /*constexpr*/ (!use_ngram_position_match) {
+          if (*l != *r) {
+            similarity = 0;
+            break;
+          }
+        } else {
+          if (*l == *r) {
+            ++similarity;
+          }
         }
       }
-      auto tmp = S[rhs_ngram_idx];
-      S[rhs_ngram_idx] =
+      if /*constexpr*/ (use_ngram_position_match) {
+        similarity = similarity / float_t(ngram_size);
+      }
+
+      auto tmp = cache[rhs_ngram_idx];
+      cache[rhs_ngram_idx] =
           std::max(
-            std::max(S[rhs_ngram_idx - 1],
-                     S[lhs_ngram_idx]),
-            prev_best + similarity);
-      prev_best = tmp;
+            std::max(cache[rhs_ngram_idx - 1],
+                     cache[lhs_ngram_idx]),
+            d + similarity);
+      d = tmp;
     }
   }
 
-  return S[rhs_ngram_length] / float_t(std::max(lhs_ngram_length, rhs_ngram_length));
+  return cache[rhs_ngram_count] / float_t(std::max(lhs_ngram_count, rhs_ngram_count));
 }
 
 NS_END
--- tests/utils/ngram_match_utils_tests.cpp
+++ tests/utils/ngram_match_utils_tests.cpp
@@ -27,61 +27,83 @@
 TEST(ngram_match_utils_test, test_similarity_empty_left) {
   const irs::string_ref lhs = ""; 
   const irs::string_ref rhs = "abcd";
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5)));
 }
 
 TEST(ngram_match_utils_test, test_similarity_empty_right) {
   const irs::string_ref lhs = "abcd"; 
   const irs::string_ref rhs = "";
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5)));
 }
 
 TEST(ngram_match_utils_test, test_similarity_no_match) {
   const irs::string_ref lhs = "abcd"; 
   const irs::string_ref rhs = "efgh";
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5)));
 }
 
 TEST(ngram_match_utils_test, test_similarity_simple) {
   const irs::string_ref lhs = "aecd"; 
   const irs::string_ref rhs = "abcd";
-  ASSERT_DOUBLE_EQ(0.75f, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1));
-  ASSERT_DOUBLE_EQ(1.f/3.f, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5));
+  ASSERT_DOUBLE_EQ(0.75f, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1)));
+  ASSERT_DOUBLE_EQ(1.f/3.f, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5)));
 }
 
 TEST(ngram_match_utils_test, test_similarity_different_length) {
   const irs::string_ref lhs = "applejuice"; 
   const irs::string_ref rhs = "aplejuice"; 
-  ASSERT_DOUBLE_EQ(0.9f, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1));
-  ASSERT_DOUBLE_EQ(8.f/9.f, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2));
-  ASSERT_DOUBLE_EQ(0.75, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3));
-  ASSERT_DOUBLE_EQ(5.f/7.f, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4));
-  ASSERT_DOUBLE_EQ(4.f/6.f, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 9));
+  ASSERT_DOUBLE_EQ(0.9f, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1)));
+  ASSERT_DOUBLE_EQ(8.f/9.f, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2)));
+  ASSERT_DOUBLE_EQ(0.75, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3)));
+  ASSERT_DOUBLE_EQ(5.f/7.f, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4)));
+  ASSERT_DOUBLE_EQ(4.f/6.f, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 9)));
 }
 
 TEST(ngram_match_utils_test, test_similarity_with_gaps) {
   const irs::string_ref lhs = "apple1234juice"; 
   const irs::string_ref rhs = "aple567juice"; 
-  ASSERT_DOUBLE_EQ(9.f/14.f, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1));
-  ASSERT_DOUBLE_EQ(7.f/13.f, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2));
-  ASSERT_DOUBLE_EQ(4.f/12.f, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3));
-  ASSERT_DOUBLE_EQ(2.f/11.f, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4));
-  ASSERT_DOUBLE_EQ(1.f/10.f, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5));
-  ASSERT_DOUBLE_EQ(0, irs::ngram_similarity(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 6));
+  ASSERT_DOUBLE_EQ(9.f/14.f, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1)));
+  ASSERT_DOUBLE_EQ(7.f/13.f, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2)));
+  ASSERT_DOUBLE_EQ(4.f/12.f, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3)));
+  ASSERT_DOUBLE_EQ(2.f/11.f, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4)));
+  ASSERT_DOUBLE_EQ(1.f/10.f, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 6)));
+}
+
+TEST(ngram_match_utils_test_with_position, test_similarity_with_gaps) {
+  const irs::string_ref lhs = "apple1234juice"; 
+  const irs::string_ref rhs = "aple567juice"; 
+  ASSERT_DOUBLE_EQ(9.f/14.f, (irs::ngram_similarity<char, true>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1)));
+  ASSERT_DOUBLE_EQ(7.f/13.f, (irs::ngram_similarity<char, true>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2)));
+  ASSERT_DOUBLE_EQ(4.f/12.f, (irs::ngram_similarity<char, true>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3)));
+  ASSERT_DOUBLE_EQ(2.f/11.f, (irs::ngram_similarity<char, true>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4)));
+  ASSERT_DOUBLE_EQ(1.f/10.f, (irs::ngram_similarity<char, true>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, false>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 6)));
+}
+
+TEST(ngram_match_utils_test_with_positions, test_similarity_with_gaps) {
+  const irs::string_ref lhs = "apple1234juice"; 
+  const irs::string_ref rhs = "aple567juice"; 
+  ASSERT_DOUBLE_EQ(9.f/14.f, (irs::ngram_similarity<char, true>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 1)));
+  ASSERT_DOUBLE_EQ(7.f/13.f, (irs::ngram_similarity<char, true>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 2)));
+  ASSERT_DOUBLE_EQ(4.f/12.f, (irs::ngram_similarity<char, true>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 3)));
+  ASSERT_DOUBLE_EQ(2.f/11.f, (irs::ngram_similarity<char, true>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 4)));
+  ASSERT_DOUBLE_EQ(1.f/10.f, (irs::ngram_similarity<char, true>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 5)));
+  ASSERT_DOUBLE_EQ(0, (irs::ngram_similarity<char, true>(lhs.begin(), lhs.size(), rhs.begin(), rhs.size(), 6)));
 }
