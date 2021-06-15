b662139a9183ef7a34dae9a6dac49ef0e802deeb
Andrei Lobov <andrei.lobov@arangodb.com>
2021-06-15 17:26:36 +0300

    fix visitor

--- core/search/disjunction.hpp
+++ core/search/disjunction.hpp
@@ -766,6 +766,9 @@ class disjunction final
   virtual void visit(void* ctx, bool (*visitor)(void*, Adapter&)) override {
     assert(ctx);
     assert(visitor);
+    if (heap_.empty()) {
+      return;
+    }
     hitch_all_iterators();
     auto& lead = itrs_[heap_.back()];
     auto cont = visitor(ctx, lead);
@@ -909,6 +912,7 @@ class disjunction final
 
   std::pair<heap_iterator, heap_iterator> hitch_all_iterators() {
     // hitch all iterators in head to the lead (current doc_)
+    assert(!heap_.empty());
     auto begin = heap_.begin(), end = heap_.end()-1;
 
     auto& doc = std::get<document>(attrs_);
