5fa7ddc5796493ddefe57c5826b3146542b314b9
Alexey Bakharew <alex.bakharew@gmail.com>
2022-02-17 13:18:23 +0300

    wip

--- tests/index/assert_format.cpp
+++ tests/index/assert_format.cpp
@@ -671,7 +671,7 @@ void assert_term(
   }
 
   if (failed_terms[0] == term_as_str || failed_terms[1] == term_as_str) {
-      std::cout << " term = " << term_as_str << " , ";
+//      std::cout << " term = " << term_as_str << " , ";
   }
 
   size_t doc_count = 0;
@@ -730,7 +730,7 @@ void assert_term(
       }
     }
   }
-  std::cout << "postings = " << doc_count << std::endl;
+//  std::cout << "postings = " << doc_count << std::endl;
   ASSERT_FALSE(actual_docs->next());
   ASSERT_TRUE(irs::doc_limits::eof(expected_docs->value()));
   ASSERT_TRUE(irs::doc_limits::eof(actual_docs->value()));
@@ -757,7 +757,7 @@ void assert_terms_next(
                              : actual_field.iterator(irs::SeekMode::NORMAL);
 
   for (; expected_term->next(); ++actual_size) {
-    std::cout << "index of term = " << actual_size << ", ";
+//    std::cout << "index of term = " << actual_size << ", ";
     ASSERT_TRUE(actual_term->next());
 
     assert_term(*expected_term, *actual_term, features);
