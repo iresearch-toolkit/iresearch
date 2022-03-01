06b2a14dfd0a438736aa0abc4475a1e27f96aebe
Andrey Abramov <andrey@arangodb.com>
2022-03-01 11:59:19 +0300

    fix mac build

--- core/store/mmap_directory.cpp
+++ core/store/mmap_directory.cpp
@@ -150,7 +150,8 @@ index_input::ptr mmap_directory::open(
     std::string_view name,
     IOAdvice advice) const noexcept {
   try {
-    const auto path = directory() / name;
+    auto path = directory();
+    path /= name;
 
     return mmap_index_input::open(path.c_str(), advice);
   } catch(...) {
