95b27575015c71af6ec01348fea25ca22d17bde6
Valery Mironov <valera.mironow@gmail.com>
2022-03-22 17:43:15 +0300

    Don't use abseil internal stuff

--- core/utils/string.hpp
+++ core/utils/string.hpp
@@ -29,8 +29,6 @@
 #include <cassert>
 #include <vector>
 
-#include <absl/hash/internal/city.h>
-
 #include "shared.hpp"
 
 // ----------------------------------------------------------------------------
