ebfb1f2e8fe17934594b44f4f5ea4fff1e0439a0
Andrey Abramov <andrey@arangodb.com>
2022-07-06 20:27:34 +0200

    convert analyzer to namespace

--- core/analysis/analyzers.hpp
+++ core/analysis/analyzers.hpp
@@ -26,112 +26,84 @@
 
 #include <functional>
 
-#include "shared.hpp"
 #include "analyzer.hpp"
-#include "utils/text_format.hpp"
+#include "shared.hpp"
 #include "utils/result.hpp"
+#include "utils/text_format.hpp"
 
-namespace iresearch {
-namespace analysis {
+namespace iresearch::analysis {
 
-// -----------------------------------------------------------------------------
-// --SECTION--                                             analyzer registration
-// -----------------------------------------------------------------------------
-
-typedef analysis::analyzer::ptr(*factory_f)(string_ref args);
-typedef bool(*normalizer_f)(string_ref args, std::string& config);
+using factory_f = analysis::analyzer::ptr (*)(string_ref args);
+using normalizer_f = bool (*)(string_ref args, std::string& config);
 
 class analyzer_registrar {
  public:
-   analyzer_registrar(
-    const type_info& type,
-    const type_info& args_format,
-    factory_f factory,
-    normalizer_f normalizer,
-    const char* source = nullptr);
+  analyzer_registrar(const type_info& type, const type_info& args_format,
+                     factory_f factory, normalizer_f normalizer,
+                     const char* source = nullptr);
 
-  operator bool() const noexcept {
-    return registered_;
-  }
+  operator bool() const noexcept { return registered_; }
 
  private:
   bool registered_;
 };
 
-#define REGISTER_ANALYZER__(analyzer_name, args_format, factory, normalizer, line, source) \
-    static ::iresearch::analysis::analyzer_registrar analyzer_registrar ## _ ## line(::iresearch::type<analyzer_name>::get(), ::iresearch::type<args_format>::get(), &factory, &normalizer, source)
-#define REGISTER_ANALYZER_EXPANDER__(analyzer_name, args_format, factory, normalizer, file, line) REGISTER_ANALYZER__(analyzer_name, args_format, factory, normalizer, line, file ":" TOSTRING(line))
-#define REGISTER_ANALYZER(analyzer_name, args_format, factory, normalizer) REGISTER_ANALYZER_EXPANDER__(analyzer_name, args_format, factory, normalizer, __FILE__, __LINE__)
-#define REGISTER_ANALYZER_JSON(analyzer_name, factory, normalizer) REGISTER_ANALYZER(analyzer_name, ::iresearch::text_format::json, factory, normalizer)
-#define REGISTER_ANALYZER_TEXT(analyzer_name, factory, normalizer) REGISTER_ANALYZER(analyzer_name, ::iresearch::text_format::text, factory, normalizer)
-#define REGISTER_ANALYZER_TYPED(analyzer_name, args_format) REGISTER_ANALYZER(analyzer_name, args_format, analyzer_name::make)
-#define REGISTER_ANALYZER_VPACK(analyzer_name, factory, normalizer) REGISTER_ANALYZER(analyzer_name, ::iresearch::text_format::vpack, factory, normalizer)
+namespace analyzers {
+// Checks whether an analyzer with the specified name is registered.
+bool exists(string_ref name, const type_info& args_format,
+            bool load_library = true);
 
-// -----------------------------------------------------------------------------
-// --SECTION--                                               convinience methods
-// -----------------------------------------------------------------------------
+// Normalize arguments for an analyzer specified by name and store them
+// in 'out' argument.
+// Returns true on success, false - otherwise
+bool normalize(std::string& out, string_ref name, const type_info& args_format,
+               string_ref args, bool load_library = true) noexcept;
 
-class analyzers {
- public:
-  ////////////////////////////////////////////////////////////////////////////////
-  /// @brief checks whether an analyzer with the specified name is registered
-  ////////////////////////////////////////////////////////////////////////////////
-  static bool exists(
-    string_ref name,
-    const type_info& args_format,
-    bool load_library = true);
+// Find an analyzer by name, or nullptr if not found
+// indirect call to <class>::make(...).
+// NOTE: make(...) MUST be defined in CPP to ensire proper code scope
+result get(analyzer::ptr& analyzer, string_ref name,
+           const type_info& args_format, string_ref args,
+           bool load_library = true) noexcept;
 
-  ////////////////////////////////////////////////////////////////////////////////
-  /// @brief normalized arguments for an analyzer specified by name and store them
-  ///        in 'out' argument
-  /// @returns true on success, false - otherwise
-  ////////////////////////////////////////////////////////////////////////////////
-  static bool normalize(
-    std::string& out,
-    string_ref name,
-    const type_info& args_format,
-    string_ref args,
-    bool load_library = true) noexcept;
+// Find an analyzer by name, or nullptr if not found
+// indirect call to <class>::make(...).
+// NOTE: make(...) MUST be defined in CPP to ensire proper code scope
+analyzer::ptr get(string_ref name, const type_info& args_format,
+                  string_ref args, bool load_library = true) noexcept;
 
-  ////////////////////////////////////////////////////////////////////////////////
-  /// @brief find an analyzer by name, or nullptr if not found
-  ///        indirect call to <class>::make(...)
-  ///        NOTE: make(...) MUST be defined in CPP to ensire proper code scope
-  ////////////////////////////////////////////////////////////////////////////////
-  static result get(
-    analyzer::ptr& analyzer,
-    string_ref name,
-    const type_info& args_format,
-    string_ref args,
-    bool load_library = true) noexcept;
+// Load all analyzers from plugins directory.
+void load_all(std::string_view path);
 
-  ////////////////////////////////////////////////////////////////////////////////
-  /// @brief find an analyzer by name, or nullptr if not found
-  ///        indirect call to <class>::make(...)
-  ///        NOTE: make(...) MUST be defined in CPP to ensire proper code scope
-  ////////////////////////////////////////////////////////////////////////////////
-  static analyzer::ptr get(
-    string_ref name,
-    const type_info& args_format,
-    string_ref args,
-    bool load_library = true) noexcept;
+// Visit all loaded analyzers, terminate early if visitor returns false.
+bool visit(const std::function<bool(string_ref, const type_info&)>& visitor);
 
-  ////////////////////////////////////////////////////////////////////////////////
-  /// @brief load all analyzers from plugins directory
-  ////////////////////////////////////////////////////////////////////////////////
-  static void load_all(std::string_view path);
+}  // namespace analyzers
+}  // namespace iresearch::analysis
 
-  ////////////////////////////////////////////////////////////////////////////////
-  /// @brief visit all loaded analyzers, terminate early if visitor returns false
-  ////////////////////////////////////////////////////////////////////////////////
-  static bool visit(
-    const std::function<bool(string_ref, const type_info&)>& visitor);
-
- private:
-  analyzers() = delete;
-};
-
-} // namespace analysis {
-}
+#define REGISTER_ANALYZER__(analyzer_name, args_format, factory, normalizer, \
+                            line, source)                                    \
+  static ::iresearch::analysis::analyzer_registrar                           \
+      analyzer_registrar##_##line(::iresearch::type<analyzer_name>::get(),   \
+                                  ::iresearch::type<args_format>::get(),     \
+                                  &factory, &normalizer, source)
+#define REGISTER_ANALYZER_EXPANDER__(analyzer_name, args_format, factory,    \
+                                     normalizer, file, line)                 \
+  REGISTER_ANALYZER__(analyzer_name, args_format, factory, normalizer, line, \
+                      file ":" TOSTRING(line))
+#define REGISTER_ANALYZER(analyzer_name, args_format, factory, normalizer) \
+  REGISTER_ANALYZER_EXPANDER__(analyzer_name, args_format, factory,        \
+                               normalizer, __FILE__, __LINE__)
+#define REGISTER_ANALYZER_JSON(analyzer_name, factory, normalizer)          \
+  REGISTER_ANALYZER(analyzer_name, ::iresearch::text_format::json, factory, \
+                    normalizer)
+#define REGISTER_ANALYZER_TEXT(analyzer_name, factory, normalizer)          \
+  REGISTER_ANALYZER(analyzer_name, ::iresearch::text_format::text, factory, \
+                    normalizer)
+#define REGISTER_ANALYZER_TYPED(analyzer_name, args_format) \
+  REGISTER_ANALYZER(analyzer_name, args_format, analyzer_name::make)
+#define REGISTER_ANALYZER_VPACK(analyzer_name, factory, normalizer)          \
+  REGISTER_ANALYZER(analyzer_name, ::iresearch::text_format::vpack, factory, \
+                    normalizer)
 
 #endif
