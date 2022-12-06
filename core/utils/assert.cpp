d3c4538880f6d22337116cadccb19d1758d56b19
Valery Mironov <valera.mironow@gmail.com>
2022-12-06 14:58:22 +0100

    Add assert statement

---
+++ core/utils/assert.cpp
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2022 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Valery Mironov
+////////////////////////////////////////////////////////////////////////////////
+
+#include "utils/assert.hpp"
+
+#include <array>
+#include <cstddef>
+
+namespace iresearch {
+namespace detail {
+
+static std::array<LogCallback, static_cast<std::size_t>(LogLevel::Count)>
+  sCallbacks = {};
+
+void LogMessage(LogLevel level, std::string_view file, std::size_t line,
+                std::string_view func, std::string_view condition) noexcept {
+  if (const auto callback = sCallbacks[static_cast<std::size_t>(level)];
+      callback != nullptr) {
+    callback(file, line, func, condition);
+  }
+}
+
+}  // namespace detail
+
+void SetCallback(LogLevel level, LogCallback callback) noexcept {
+  detail::sCallbacks[static_cast<std::size_t>(level)] = callback;
+}
+
+}  // namespace iresearch
---
+++ core/utils/assert.hpp
@@ -0,0 +1,83 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2022 ArangoDB GmbH, Cologne, Germany
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Valery Mironov
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <cstddef>
+#include <string_view>
+
+namespace iresearch {
+
+enum class LogLevel : unsigned char {
+  Assert = 0,
+  Count = 1,
+};
+
+namespace detail {
+
+void LogMessage(LogLevel level, std::string_view file, std::size_t line,
+                std::string_view func, std::string_view condition) noexcept;
+
+}
+
+using LogCallback = void (*)(std::string_view file, std::size_t line,
+                             std::string_view function,
+                             std::string_view condition) noexcept;
+
+void SetCallback(LogLevel level, LogCallback callback) noexcept;
+
+}  // namespace iresearch
+
+#ifndef IRS_FUNC_NAME
+#if defined(__clang__) || defined(__GNUC__)
+#define IRS_FUNC_NAME __PRETTY_FUNCTION__
+#elif defined(_MSC_VER)
+#define IRS_FUNC_NAME __FUNCSIG__
+#else
+#define IRS_FUNC_NAME __func__
+#endif
+#endif
+
+#define IRS_LOG_MESSAGE(level, cond)                                      \
+  do {                                                                    \
+    if (!!(cond)) {                                                       \
+      ::IRS::detail::LogMessage(level, __FILE__, __LINE__, IRS_FUNC_NAME, \
+                                #cond);                                   \
+    }                                                                     \
+  } while (false)
+
+#ifdef NDEBUG
+#define IRS_STUB1(first) ((void)1)
+#else
+#define IRS_STUB1(first) \
+  do {                   \
+    if (false) {         \
+      (void)(first);     \
+    }                    \
+  } while (false)
+#endif
+
+#ifdef IRESEARCH_DEBUG
+#define IRS_ASSERT(cond) IRS_LOG_MESSAGE(::iresearch::LogLevel::Assert, !(cond))
+#else
+#define IRS_ASSERT(cond) IRS_STUB1(!(cond))
+#endif
