da4c828ce667bcbd9f643dfeee4582e6bd3b0de7
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2023-08-15 18:24:03 +0200

    Fix

--- tests/search/wand_test.cpp
+++ tests/search/wand_test.cpp
@@ -111,13 +111,54 @@ class WandTestCase : public tests::index_test_base {
                        bool append_data = false);
   void GenerateSegmentMinNorm(irs::ScorersView scorers);
   void ConsolidateAll(irs::ScorersView scorers, bool write_norms);
+
+  void AssertFilters(irs::ScorersView scorers) {
+    auto apply = [&](auto assert_filter) {
+      ASSERT_FALSE(scorers.empty());
+      for (size_t idx = 0; auto* scorer : scorers) {
+        std::invoke(assert_filter, *this, scorers, *scorer, idx);
+        ++idx;
+      }
+      // Invalid scorer
+      std::invoke(assert_filter, *this, scorers, *scorers[0], scorers.size());
+    };
+    apply(&WandTestCase::AssertTermFilter);
+    apply(&WandTestCase::AssertConjunctionFilter);
+    apply(&WandTestCase::AssertDisjunctionFilter);
+  }
+
   void AssertTermFilter(irs::ScorersView scorers, const irs::Scorer& scorer,
                         irs::byte_type wand_index);
-  void AssertTermFilter(irs::ScorersView scorers);
+
   void AssertConjunctionFilter(irs::ScorersView scorers,
                                const irs::Scorer& scorer,
                                irs::byte_type wand_index);
-  void AssertConjunctionFilter(irs::ScorersView scorers);
+
+  void AssertDisjunctionFilter(irs::ScorersView scorers,
+                               const irs::Scorer& scorer,
+                               irs::byte_type wand_index);
+
+  bool CanUseWand(irs::ScorersView scorers, const irs::Scorer& scorer,
+                  irs::byte_type wand_index, const irs::term_reader& field) {
+    const auto& field_meta = field.meta();
+    const auto index_features = scorer.index_features();
+    if (index_features != (index_features & field_meta.index_features)) {
+      return false;
+    }
+
+    irs::feature_set_t features;
+    scorer.get_features(features);
+
+    for (const auto feature : features) {
+      const auto it = field_meta.features.find(feature);
+      if (it == field_meta.features.end() ||
+          !irs::field_limits::valid(it->second)) {
+        return false;
+      }
+    }
+
+    return wand_index < scorers.size();
+  }
 };
 
 std::vector<Doc> WandTestCase::Collect(const irs::DirectoryReader& index,
@@ -279,26 +320,6 @@ void WandTestCase::GenerateSegmentMinNorm(irs::ScorersView scorers) {
   add_segment(gen, open_mode, GetWriterOptions(scorers, true));
 }
 
-void WandTestCase::AssertTermFilter(irs::ScorersView scorers) {
-  ASSERT_FALSE(scorers.empty());
-  for (size_t idx = 0; auto* scorer : scorers) {
-    AssertTermFilter(scorers, *scorer, idx);
-    ++idx;
-  }
-  // Invalid scorer
-  AssertTermFilter(scorers, *scorers[0], scorers.size());
-}
-
-void WandTestCase::AssertConjunctionFilter(irs::ScorersView scorers) {
-  ASSERT_FALSE(scorers.empty());
-  for (size_t idx = 0; auto* scorer : scorers) {
-    AssertConjunctionFilter(scorers, *scorer, idx);
-    ++idx;
-  }
-  // Invalid scorer
-  AssertConjunctionFilter(scorers, *scorers[0], scorers.size());
-}
-
 void WandTestCase::AssertTermFilter(irs::ScorersView scorers,
                                     const irs::Scorer& scorer,
                                     irs::byte_type wand_index) {
@@ -315,27 +336,7 @@ void WandTestCase::AssertTermFilter(irs::ScorersView scorers,
     const auto* field = segment.field(kFieldName);
     ASSERT_NE(nullptr, field);
 
-    const auto can_use_wand = [&]() {
-      const auto& field_meta = field->meta();
-      const auto index_features = scorer.index_features();
-      if (index_features != (index_features & field_meta.index_features)) {
-        return false;
-      }
-
-      irs::feature_set_t features;
-      scorer.get_features(features);
-
-      for (const auto feature : features) {
-        const auto it = field_meta.features.find(feature);
-        if (it == field_meta.features.end() ||
-            !irs::field_limits::valid(it->second)) {
-          return false;
-        }
-      }
-
-      return wand_index < scorers.size();
-    }();
-
+    const auto can_use_wand = CanUseWand(scorers, scorer, wand_index, *field);
     ASSERT_EQ(can_use_wand, field->has_scorer(wand_index));
 
     for (auto terms = field->iterator(irs::SeekMode::NORMAL); terms->next();) {
@@ -353,8 +354,8 @@ void WandTestCase::AssertConjunctionFilter(irs::ScorersView scorers,
   static constexpr std::string_view kFieldName = "name";
 
   irs::And conjunction;
-  irs::by_term& filter = conjunction.add<irs::by_term>();
-  *filter.mutable_field() = kFieldName;
+  irs::by_term& filter1 = conjunction.add<irs::by_term>();
+  *filter1.mutable_field() = kFieldName;
   irs::by_term& filter2 = conjunction.add<irs::by_term>();
   *filter2.mutable_field() = kFieldName;
 
@@ -366,35 +367,54 @@ void WandTestCase::AssertConjunctionFilter(irs::ScorersView scorers,
     const auto* field = segment.field(kFieldName);
     ASSERT_NE(nullptr, field);
 
-    const auto can_use_wand = [&]() {
-      const auto& field_meta = field->meta();
-      const auto index_features = scorer.index_features();
-      if (index_features != (index_features & field_meta.index_features)) {
-        return false;
-      }
-
-      irs::feature_set_t features;
-      scorer.get_features(features);
-
-      for (const auto feature : features) {
-        const auto it = field_meta.features.find(feature);
-        if (it == field_meta.features.end() ||
-            !irs::field_limits::valid(it->second)) {
-          return false;
-        }
-      }
-
-      return wand_index < scorers.size();
-    }();
-
+    const auto can_use_wand = CanUseWand(scorers, scorer, wand_index, *field);
     ASSERT_EQ(can_use_wand, field->has_scorer(wand_index));
+
     auto terms = field->iterator(irs::SeekMode::NORMAL);
     ASSERT_TRUE(terms->next());
-    filter.mutable_options()->term = terms->value();
+    filter1.mutable_options()->term = terms->value();
     ASSERT_TRUE(terms->next());
     filter2.mutable_options()->term = terms->value();
+
     AssertResults(reader, conjunction, scorers, wand_index, can_use_wand, 10);
     AssertResults(reader, conjunction, scorers, wand_index, can_use_wand, 100);
+  }
+}
+
+void WandTestCase::AssertDisjunctionFilter(irs::ScorersView scorers,
+                                           const irs::Scorer& scorer,
+                                           irs::byte_type wand_index) {
+  static constexpr std::string_view kFieldName = "name";
+
+  irs::Or disjunction;
+  irs::by_term& filter1 = disjunction.add<irs::by_term>();
+  *filter1.mutable_field() = kFieldName;
+  irs::by_term& filter2 = disjunction.add<irs::by_term>();
+  *filter2.mutable_field() = kFieldName;
+  irs::by_term& filter3 = disjunction.add<irs::by_term>();
+  *filter3.mutable_field() = kFieldName;
+
+  auto reader = irs::DirectoryReader{
+    dir(), codec(), irs::IndexReaderOptions{.scorers = scorers}};
+  ASSERT_NE(nullptr, reader);
+
+  for (const auto& segment : reader) {
+    const auto* field = segment.field(kFieldName);
+    ASSERT_NE(nullptr, field);
+
+    const auto can_use_wand = CanUseWand(scorers, scorer, wand_index, *field);
+    ASSERT_EQ(can_use_wand, field->has_scorer(wand_index));
+
+    auto terms = field->iterator(irs::SeekMode::NORMAL);
+    ASSERT_TRUE(terms->next());
+    filter1.mutable_options()->term = terms->value();
+    ASSERT_TRUE(terms->next());
+    filter2.mutable_options()->term = terms->value();
+    ASSERT_TRUE(terms->next());
+    filter3.mutable_options()->term = terms->value();
+
+    AssertResults(reader, disjunction, scorers, wand_index, can_use_wand, 10);
+    AssertResults(reader, disjunction, scorers, wand_index, can_use_wand, 100);
   }
 }
 
@@ -410,53 +430,54 @@ TEST_P(WandTestCase, TermFilterMultipleScorersDense) {
   ASSERT_TRUE(bm11.IsBM11());
 
   GenerateSegment(scorers, true);
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
 
   GenerateSegment(scorers, true, true);  // Add another segment
   ConsolidateAll(scorers, true);
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
 
   GenerateSegment(scorers, true, true);  // Add another segment
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
+
   GenerateSegment(scorers, true, true);  // Add another segment
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
+
   GenerateSegment(scorers, true, true);  // Add another segment
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
+
   ConsolidateAll(scorers, true);
-
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
 }
 
-TEST_P(WandTestCase, ConjunctionFilterMultipleScorersDense) {
+TEST_P(WandTestCase, TermFilterManyScorersDense) {
   Scorers scorers;
   scorers.PushBack<irs::TFIDF>(false);
   scorers.PushBack<irs::TFIDF>(true);
-  const auto& bm25 = scorers.PushBack<irs::BM25>();
-  ASSERT_FALSE(bm25.IsBM15() || bm25.IsBM11());
-  const auto& bm15 = scorers.PushBack<irs::BM25>(irs::BM25::K(), 0.f);
-  ASSERT_TRUE(bm15.IsBM15());
-  const auto& bm11 = scorers.PushBack<irs::BM25>(irs::BM25::K(), 1.f);
-  ASSERT_TRUE(bm11.IsBM11());
+  scorers.PushBack<irs::BM25>();
+  scorers.PushBack<irs::BM25>(irs::BM25::K(), 0.0f);
+  scorers.PushBack<irs::BM25>(irs::BM25::K(), 1.0f);
   scorers.PushBack<irs::BM25>(irs::BM25::K(), 0.4f);
   scorers.PushBack<irs::BM25>(irs::BM25::K(), 0.2f);
   scorers.PushBack<irs::BM25>(irs::BM25::K(), 0.1f);
 
   GenerateSegment(scorers, true);
-  AssertConjunctionFilter(scorers);
+  AssertFilters(scorers);
 
   GenerateSegment(scorers, true, true);  // Add another segment
   ConsolidateAll(scorers, true);
-  AssertConjunctionFilter(scorers);
+  AssertFilters(scorers);
 
   GenerateSegment(scorers, true, true);  // Add another segment
-  AssertConjunctionFilter(scorers);
+  AssertFilters(scorers);
+
   GenerateSegment(scorers, true, true);  // Add another segment
-  AssertConjunctionFilter(scorers);
+  AssertFilters(scorers);
+
   GenerateSegment(scorers, true, true);  // Add another segment
-  AssertConjunctionFilter(scorers);
+  AssertFilters(scorers);
+
   ConsolidateAll(scorers, true);
-
-  AssertConjunctionFilter(scorers);
+  AssertFilters(scorers);
 }
 
 TEST_P(WandTestCase, TermFilterMultipleScorersSparse) {
@@ -471,13 +492,13 @@ TEST_P(WandTestCase, TermFilterMultipleScorersSparse) {
   ASSERT_TRUE(bm11.IsBM11());
 
   GenerateSegment(scorers, false);
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
 
   GenerateSegment(scorers, false, true);  // Add another segment
-  AssertTermFilter(scorers);
-  ConsolidateAll(scorers, false);
+  AssertFilters(scorers);
 
-  AssertTermFilter(scorers);
+  ConsolidateAll(scorers, false);
+  AssertFilters(scorers);
 }
 
 TEST_P(WandTestCase, TermFilterTFIDF) {
@@ -485,7 +506,7 @@ TEST_P(WandTestCase, TermFilterTFIDF) {
   scorers.PushBack<irs::TFIDF>(false);
 
   GenerateSegment(scorers, true);
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
 }
 
 TEST_P(WandTestCase, TermFilterTFIDFWithNorms) {
@@ -493,7 +514,7 @@ TEST_P(WandTestCase, TermFilterTFIDFWithNorms) {
   scorers.PushBack<irs::TFIDF>(true);
 
   GenerateSegment(scorers, true);
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
 }
 
 TEST_P(WandTestCase, TermFilterBM25) {
@@ -503,9 +524,10 @@ TEST_P(WandTestCase, TermFilterBM25) {
   ASSERT_FALSE(scorer.IsBM11());
 
   GenerateSegment(scorers, true);
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
+
   GenerateSegmentMinNorm(scorers);
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
 }
 
 TEST_P(WandTestCase, TermFilterBM15) {
@@ -514,7 +536,7 @@ TEST_P(WandTestCase, TermFilterBM15) {
   ASSERT_TRUE(scorer.IsBM15());
 
   GenerateSegment(scorers, true);
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
 }
 
 TEST_P(WandTestCase, TermFilterBM11) {
@@ -523,7 +545,7 @@ TEST_P(WandTestCase, TermFilterBM11) {
   ASSERT_TRUE(scorer.IsBM11());
 
   GenerateSegment(scorers, true);
-  AssertTermFilter(scorers);
+  AssertFilters(scorers);
 }
 
 static constexpr auto kTestDirs = tests::getDirectories<tests::kTypesDefault>();
