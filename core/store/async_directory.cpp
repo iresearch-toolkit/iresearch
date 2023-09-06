c03af209a1604b3b9af84797b63731dc83ea805a
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2023-09-06 12:08:58 +0200

    Fix build with or without liburing (#554)

--- core/store/async_directory.cpp
+++ core/store/async_directory.cpp
@@ -466,10 +466,12 @@ void AsyncIndexOutput::write_bytes(const byte_type* b, size_t length) {
   }
 }
 
-AsyncDirectory::AsyncDirectory(std::filesystem::path path,
-                               directory_attributes attrs, size_t pool_size,
-                               size_t queue_size, unsigned flags)
-  : MMapDirectory{std::move(path), std::move(attrs)},
+AsyncDirectory::AsyncDirectory(std::filesystem::path dir,
+                               directory_attributes attrs,
+                               const ResourceManagementOptions& rm,
+                               size_t pool_size, size_t queue_size,
+                               unsigned flags)
+  : MMapDirectory{std::move(dir), std::move(attrs), rm},
     async_pool_{pool_size},
     queue_size_{queue_size},
     flags_{flags} {}
